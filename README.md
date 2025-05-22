39f27f771539d08074a05c961fc49eab668ed7c9
Jeremy Wood <jeremy.wood@7factor.io>
2025-05-22 14:16:58 -0400

    Initial pass at s3 backend action.

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # tfstate-s3-backend-action
 This GitHub Action will aid in creating a terraform state backend in AWS S3.
+
+## Usage
+You're free to use this action however you want. However, here's a example workflow that uses `workflow_dispatch`.
+
+```yaml
+name: Terraform S3 Backend Bucket
+
+on:
+  workflow_dispatch:
+    inputs:
+      account:
+        description: 'AWS Account'
+        required: true
+        type: choice
+        options:
+          - admin
+          - dev
+          - prod
+      first_run:
+        description: 'First run?'
+        required: true
+        type: boolean
+        default: false
+
+jobs:
+  terraform_s3_bucket:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Configure AWS credentials
+        uses: aws-actions/configure-aws-credentials@v4
+        with:
+          aws-access-key-id: ${{ secrets[format('{0}_ACCESS_KEY_ID', inputs.account)] }}
+          aws-secret-access-key: ${{ secrets[format('{0}_SECRET_ACCESS_KEY', inputs.account)] }}
+          aws-region: us-east-1
+
+      - name: Terraform
+        uses: 7Factor/tfstate-s3-backend-action@v1
+        with:
+          first_run: ${{ inputs.first_run }}
+          workspace: terraform-state-bucket
+          s3_bucket_name: ${{ vars.ORGANIZATION }}-${{ inputs.account }}-terraform-state
+```
---
+++ terraform/main.tf
@@ -0,0 +1,141 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.s3_bucket_name
+}
+
+resource "aws_s3_bucket_public_access_block" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+data "aws_iam_policy_document" "bucket_policy" {
+  statement {
+    sid = "DenyIncorrectEncryptionHeader"
+
+    effect = "Deny"
+
+    principals {
+      identifiers = ["*"]
+      type        = "AWS"
+    }
+
+    actions = [
+      "s3:PutObject"
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.s3_bucket_name}/*"
+    ]
+
+    condition {
+      test     = "StringNotEquals"
+      variable = "s3:x-amz-server-side-encryption"
+
+      values = [
+        "AES256",
+        "aws:kms"
+      ]
+    }
+  }
+
+  statement {
+    sid = "DenyUnencryptedObjectUploads"
+
+    effect = "Deny"
+
+    principals {
+      identifiers = ["*"]
+      type        = "AWS"
+    }
+
+    actions = [
+      "s3:PutObject"
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.s3_bucket_name}/*"
+    ]
+
+    condition {
+      test     = "Null"
+      variable = "s3:x-amz-server-side-encryption"
+
+      values = [
+        "true"
+      ]
+    }
+  }
+
+  statement {
+    sid = "DenyNonSSLRequests"
+
+    effect = "Deny"
+
+    principals {
+      identifiers = ["*"]
+      type        = "AWS"
+    }
+
+    actions = [
+      "s3:*"
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.s3_bucket_name}",
+      "arn:aws:s3:::${var.s3_bucket_name}/*"
+    ]
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+
+      values = [
+        "false"
+      ]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  policy = data.aws_iam_policy_document.bucket_policy.json
+
+  depends_on = [aws_s3_bucket_public_access_block.terraform_state]
+}
+
+resource "time_sleep" "wait_for_aws_s3_settings_eventual_consistency" {
+  create_duration  = "30s"
+  destroy_duration = "30s"
+
+  depends_on = [aws_s3_bucket_public_access_block.terraform_state, aws_s3_bucket_policy.terraform_state]
+}
+
+resource "aws_s3_bucket_ownership_controls" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+
+  depends_on = [time_sleep.wait_for_aws_s3_settings_eventual_consistency]
+}
+
+resource "aws_s3_bucket_acl" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  acl = "private"
+
+  depends_on = [aws_s3_bucket_ownership_controls.terraform_state]
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
